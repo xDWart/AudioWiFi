765b7513becfe33584250678a8cbe11babf498e9
oWart <owart@bk.ru>
2017-03-08 09:09:03 +0300

    Update stack/heap size

--- EWARM/settings/AudioWiFi.AudioWiFi.cspy.bat
+++ EWARM/settings/AudioWiFi.AudioWiFi.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "D:\STM32\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.general.xcl" --backend -f "D:\STM32\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.7\common\bin\cspybat" -f "C:\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.general.xcl" --backend -f "C:\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "D:\STM32\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.general.xcl" "--debug_file=%~1" --backend -f "D:\STM32\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.7\common\bin\cspybat" -f "C:\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.general.xcl" "--debug_file=%~1" --backend -f "C:\AudioWiFi\EWARM\settings\AudioWiFi.AudioWiFi.driver.xcl" 
 
 @echo off 
 :end
